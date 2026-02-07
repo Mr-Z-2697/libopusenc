22491fa53452dff60d24bec96edbcb36de2c1fa7
Mark Harris <mark.hsj@gmail.com>
2026-02-07 11:02:59 -0800

    Improve error handling in encode_buffer()
    If an unrecoverable error is encountered writing the headers for a
    chained stream, do not continue writing audio packets.
    
    If the close callback is called and returns an error, do not call it
    again in ope_encoder_destroy().
    
    Consolidate advance to next chained stream.

--- src/opusenc.c
+++ src/opusenc.c
@@ -668,8 +668,8 @@ static void encode_buffer(OggOpusEnc *enc) {
       }
       if (e_o_s) {
         EncStream *tmp;
-        tmp = enc->streams->next;
         if (enc->streams->close_at_end && !enc->pull_api) {
+          enc->streams->close_at_end = 0;
           ret = enc->callbacks.close(enc->streams->user_data);
           if (ret) {
             enc->unrecoverable = OPE_CLOSE_FAIL;
@@ -677,10 +677,11 @@ static void encode_buffer(OggOpusEnc *enc) {
             return;
           }
         }
+        tmp = enc->streams->next;
         stream_destroy(enc->streams);
         enc->streams = tmp;
-        if (!tmp) enc->last_stream = NULL;
-        if (enc->last_stream == NULL) {
+        if (!tmp) {
+          enc->last_stream = NULL;
           free(packet_copy);
           return;
         }
@@ -692,6 +693,10 @@ static void encode_buffer(OggOpusEnc *enc) {
           enc->streams->granule_offset -= enc->frame_size;
         }
         init_stream(enc);
+        if (enc->unrecoverable) {
+          free(packet_copy);
+          return;
+        }
         if (enc->chaining_keyframe) {
           unsigned char *p;
           opus_int64 granulepos2=enc->curr_granule - enc->streams->granule_offset - enc->frame_size;
